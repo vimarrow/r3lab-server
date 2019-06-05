0c231ab60e680b959402c4a7229e78aa5601a14a
abratule <abratule@cisco.com>
2019-06-05 17:01:00 +0300

    added error example and beforeMethod methods

--- example/app.js
+++ example/app.js
@@ -1,4 +1,4 @@
-const { Server, Route } = require('../index.js');
+const { Server, Route, ServerError } = require('../index.js');
 
 class PingRoute extends Route {
 	constructor(...args){
@@ -6,22 +6,57 @@ class PingRoute extends Route {
 	}
 
 	before() {
-		return true;
+		// nothing to do here
+	}
+
+	isValidPayload() { 
+		// just a custom validation method. 
+		// not part of r3lab
+		return typeof this.body.showPing === 'boolean';
+	}
+
+	beforePOST() {
+		if(this.isValidPayload()){
+			return "is doesn't really matter what";
+		} else {
+			throw ServerError(400, 'Bad Request', 'invalid Payload. showPing needs to be boolean.');
+		};
 	}
 
 	get() {
 		this.status = 200;
-		console.log(this.query, this.body);
-		console.log(asd);
-		return {ping: true};
+		console.log(this.query, this.body, this.params);
+		return {
+			ping: true,
+			id: this.params.id
+		};
 	}
+
+	post() {
+		this.status = 200;
+		return {
+			showPing: this.body.showPing
+		};
+	}
+
+	onError(error) {
+		// error is instance of ServerError
+		// internally I do this:
+		console.error(error);
+		this.status = error.status || 500;
+		return {
+			name: error.name,
+			message: error.message,
+			in: this.__getName()
+		};
+	}
+
 }
 
 const serverConfig = {
-	routes: [{
-			path: '/ping',
-			controller: PingRoute
-	}]
+	routes: {
+		'/ping/:id?': PingRoute
+	}
 };
 
 const app = new Server(serverConfig);
